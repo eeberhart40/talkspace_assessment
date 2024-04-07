ff8eb9b553279471309993e0642a16ceee8d2a77
Eric Eberhart <el.eberhart40@gmail.com>
2024-04-07 13:19:07 -0600

    Use env variables

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,12 +1,13 @@
 import { Sequelize } from "sequelize";
+require("dotenv").config();
 
 // Create a single persistent MySQL database connection pool
 const sequelize = new Sequelize(
-  "your_db_name",
-  "your_db_user",
-  "your_db_password",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: "localhost",
+    host: process.env.DB_HOST,
     dialect: "mysql", // Use the appropriate database dialect
     logging: false, // Disable logging SQL queries (you can enable it for debugging)
   }
